fbdfd87fbe7ae4ea5afb197a6accb59f492b4301
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-05-10 14:35:08 +0000

    Create index.html view

--- schlosskulm/urls.py
+++ schlosskulm/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home import views as index_views
+# from home import views as index_views
 
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    # path('', index_views.index, name='index'),
+    path('', include('home.urls'), name='home-urls'),
     path('admin/', admin.site.urls),
 ]
