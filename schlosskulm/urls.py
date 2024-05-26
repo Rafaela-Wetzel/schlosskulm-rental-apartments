17387142583d66f2f9a8647d64069be09afd6912
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-05-26 15:04:26 +0000

    Set debug to false

--- schlosskulm/urls.py
+++ schlosskulm/urls.py
@@ -4,15 +4,15 @@ from home import views
 
 urlpatterns = [
     path('', views.main_page, name='main-page'),
+    path('about-us/', views.about_us_page, name='about-us'),
     path('admin/', admin.site.urls),
     path('booking/', views.booking_page, name='booking'),
-    path('house/', views.house_page, name='house'),
-    path('directions/', views.directions_page, name='directions'),
-    path('house/upper-apartment/', views.upper_apartment_page, name='upper-apartment'),
-    path('house/lower-apartment/', views.lower_apartment_page, name='lower-apartment' ),
-    path('house/house-rules/', views.house_rules_page, name='house-rules'),
     path('contact/', views.contact_page, name='contact'),
-    path('schlosskulm/gallery/', views.gallery_page, name='gallery'),
+    path('house/', views.house_page, name='house'),
+    path('house/house-rules/', views.house_rules_page, name='house-rules'),
+    path('house/lower-apartment/', views.lower_apartment_page, name='lower-apartment' ),
+    path('house/upper-apartment/', views.upper_apartment_page, name='upper-apartment'),
     path('schlosskulm/day-trips/', views.day_trips_page, name='day-trips'),
-    path('about-us/', views.about_us_page, name='about-us'),
+    path('schlosskulm/directions/', views.directions_page, name='directions'),
+    path('schlosskulm/gallery/', views.gallery_page, name='gallery'),   
 ]
