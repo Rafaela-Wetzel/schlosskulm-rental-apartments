737fc22cc4311037c7647fe2cc73aeca5045faf0
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-05-28 15:04:25 +0000

    Set debug to false

--- schlosskulm/settings.py
+++ schlosskulm/settings.py
@@ -17,7 +17,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-rafaelawetz-schlosskulm-la2h035ybbw.ws-eu114.gitpod.io',
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
