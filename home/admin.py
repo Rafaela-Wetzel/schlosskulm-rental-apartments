2468afd4f1b2be528de59a7534f99b5dcfa1bda7
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-05-28 21:11:31 +0000

    Add single fields to bookingform meta class in forms.py

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Booking
+from home.models import Booking
 
 @admin.register(Booking)
 class PostAdmin(SummernoteModelAdmin):
--- home/forms.py
+++ home/forms.py
@@ -4,5 +4,5 @@ from .models import Booking
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = "__all__"
+        fields = ('first_name', 'last_name', 'birth_date', 'email', 'phone_number', 'address', 'zip_code', 'city', 'country', 'booking_item', 'arrival_date', 'departure_date', 'amount_guests', 'nationality', 'passport_number', 'animals', 'message')
 
