9c561572504c4ca16d7978551ee8813293e45daf
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-06-05 17:32:12 +0000

    Collect static files

--- home/urls.py
+++ home/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('your-bookings/', views.BookingList.as_view(), name='your-bookings'), 
     path('all-bookings/', views.AllBookingsList.as_view(), name='all-bookings'),
-    #path('bookings/edit_booking/<int:booking_id>',
-    #     views.booking_edit, name='booking_edit'),
+    #path('cancel_booking/<int:booking_id>', views.cancel_booking, name='cancel_booking'),
 ]
