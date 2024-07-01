b0e018e3c44d3349d3069c3d9ed94845364512c3
Rafaela Wetzel <RafaelaWetzel@gmx.de>
2024-07-01 09:49:17 +0000

    Collect static files

--- staticfiles/js/manage-bookings.js
+++ staticfiles/js/manage-bookings.js
@@ -1,68 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  const confirmButtons = document.getElementById('confirmButton');
-  const cancelButtons = document.getElementById('cancelButton');
-  const deleteButtons = document.getElementById('deleteButton');
+  const confirmButtons = document.getElementsByClassName('confirm-button');
+  const cancelButtons = document.getElementsByClassName('cancel-button');
+  const deleteButtons = document.getElementsByClassName('delete-button');
   const bookingForm = document.getElementById('bookingForm');
 
-/**
- * Functionality to confirm a booking by clicking confirm button
- */
-  for (let button of confirmButtons) {
-    button.addEventListener("click", (e) => {
+  /**
+   * Functionality to confirm a booking by clicking confirm button
+   */
+  for (var i = 0; i < confirmButtons.length; i++) {
 
-      // get booking id from dataset of button clicked
-      let bookingId = button.dataset.booking_id;
-      // insert inner text to cancelled booking button
-      button.innerText = "Confirmed!";
-      // read current URL
+    confirmButtons[i].addEventListener("click", (event) => {
+      console.log("Confirm button has been clicked!");
+      // Prevent default behavior of the click event
+      event.preventDefault();
+      // Access the event target
+      const target = event.target;
+      // Get booking id from dataset of button clicked
+      let bookingId = target.dataset.booking_id;
+      // Insert inner text to cancelled booking button
+      target.innerText = "Confirmed!";
+      // Read current URL
       let currentUrl = window.location.href;
-      // remove unused part of URL
-      newUrl = currentUrl.replace('/all-bookings/', '');
-      // redirect to cancel booking URL with booking number as argument
+      // Remove unused part of URL
+      let newUrl = currentUrl.replace('/all-bookings/', '');
+      // Redirect to confirm booking URL with booking number as argument
       window.location.href = `${newUrl}/confirm_booking/${bookingId}`;
     });
-  }
+  };
 
 
 /**
  * Functionality to cancel a booking by clicking cancel button
  */
-    for (let button of cancelButtons) {
-      button.addEventListener("click", (e) => {
-  
-        let bookingId = button.dataset.booking_id;
-        button.innerText = "Cancelled!";
-        let currentUrl = window.location.href;
-        var newUrl = '';
-        if (currentUrl.includes('your-bookings')) {
-          newUrl = currentUrl.replace('/your-bookings/', '');
-        } else {
-          newUrl = currentUrl.replace('/all-bookings/', '');
-        }
-        window.location.href = `${newUrl}/cancel_booking/${bookingId}`;
-  
-      });
-    }
+for (var i = 0; i < cancelButtons.length; i++) {
+
+  cancelButtons[i].addEventListener("click", (event) => {
+  console.log("Cancel button has been clicked!");
+  event.preventDefault();
+  const target = event.target;
+  let bookingId = target.dataset.booking_id;
+  target.innerText = "Cancelled!";
+  let currentUrl = window.location.href;
+  var newUrl = '';
+  if (currentUrl.includes('your-bookings')) {
+    newUrl = currentUrl.replace('/your-bookings/', '');
+  } else {
+    newUrl = currentUrl.replace('/all-bookings/', '');
+  }
+  window.location.href = `${newUrl}/cancel_booking/${bookingId}`;
+});
+};
 
 
 /**
  * Functionality to delete a booking by clicking delete button
  */
-  for (let button of deleteButtons) {
-    button.addEventListener("click", (e) => {
+for (var i = 0; i < deleteButtons.length; i++) {
 
-      let bookingId = button.dataset.booking_id;
-      button.innerText = "Deleted!";
-      let currentUrl = window.location.href;
-      var newUrl = '';
-      if (currentUrl.includes('your-bookings')) {
-        newUrl = currentUrl.replace('/your-bookings/', '');
-      } else {
-        newUrl = currentUrl.replace('/all-bookings/', '');
-      }
-      window.location.href = `${newUrl}/delete_booking/${bookingId}`;
-    });
+  deleteButtons[i].addEventListener("click", (event) => {
+  console.log("Delete button has been clicked!");
+  event.preventDefault();
+  const target = event.target;
+  let bookingId = target.dataset.booking_id;
+  target.innerText = "Deleted!";
+  let currentUrl = window.location.href;
+  var newUrl = '';
+  if (currentUrl.includes('your-bookings')) {
+    newUrl = currentUrl.replace('/your-bookings/', '');
+  } else {
+    newUrl = currentUrl.replace('/all-bookings/', '');
   }
+  window.location.href = `${newUrl}/delete_booking/${bookingId}`;
+});
+};
 
 });
